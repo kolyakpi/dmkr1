99d46f2e26aede3d8ed22fd65750b7ef8397b142
kolyakpi <91086565+kolyakpi@users.noreply.github.com>
2021-11-07 07:08:21 +0200

    Update HangMan.py
    Змініть репозиторій до файлу зі словами, будь ласка.

--- HangMan.py
+++ HangMan.py
@@ -60,7 +60,7 @@ def is_word_guessed(secret_word, letters_guessed):
     secret_word: string, the word the user is guessing; assumes all letters are
     lowercase
     letters_guessed: list (of letters), which letters have been guessed so far;
-    assumes that all letters are lowercase
+    assumes that all letters are lowercase 
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
     False otherwise
   '''
